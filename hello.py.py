f4dee5cac319abc4735bde44584e35499feb521a
ericcooper1 <67962236+ericcooper1@users.noreply.github.com>
2020-07-08 11:57:36 -0400

    Add files via upload

---
+++ hello.py.py
@@ -0,0 +1,22 @@
+color = "green"
+message = "i like to see %s humans and dogs to eat like guinny pigs, yummy."
+print(message % color)
+
+letter = "t"
+print(letter*10)
+
+list1 = [ 'me','you','thou']
+list2 = [ 'ty','uei','pooopypie']
+print(list1,list2)
+
+list1.append('ccc')
+print(list1)
+list2.append('i need to take a dump')
+print(list2)
+
+del list2[3]
+del list1[3]
+print(list1+list2)
+
+numlist = [1,2,3]
+print(numlist*8)
