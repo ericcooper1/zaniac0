0a066ef79b011f4b606071b37b349a9ce8fade5c
ericcooper1 <67962236+ericcooper1@users.noreply.github.com>
2020-07-09 10:15:30 -0400

    Add files via upload

---
+++ typoz.com.com.com.com.com.com.com.py
@@ -0,0 +1,24 @@
+list1 = ['U','wl3','hi']
+list1.append("848")
+print(list1)
+list2 = ['tiuy','zorg','astye']
+print(list2)
+del list1[3]
+print(list1+list2)
+
+
+MMMMMerp = (  'caaat', 'biird', 'huuuuman', 'thiiiiing' )
+print(MMMMMerp)
+print(MMMMMerp[0])
+print(MMMMMerp[0:2])
+print(MMMMMerp[0],MMMMMerp[3])
+
+
+map = { 'human' : 'thing', 'barf' : 'uuuase', 'shoot' : 'reey', 'poop' : 'doop'}
+print(map)
+del map['human']
+print(map)
+
+map['human'] = 'thing'
+map['shoot'] = 'tttttt'
+print(map)
