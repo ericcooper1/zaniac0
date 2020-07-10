89e02cb51032178c05f3aa6245356e893067af23
ericcooper1 <67962236+ericcooper1@users.noreply.github.com>
2020-07-10 09:49:33 -0400

    Add files via upload

---
+++ t.py2.py
@@ -0,0 +1,14 @@
+import turtle
+t= turtle.Pen()
+t.speed(10000000000000000000000000000000000000000)
+for x in range(135):
+    t.forward(46)
+    t.left(53.634572185)
+    t.forward(46)
+    t.left(26.953)
+    t.forward(46)
+    t.left(11.776)
+    t.forward(46)
+    t.left(5.664)
+    t.forward(46)
+    t.left(3.435)
