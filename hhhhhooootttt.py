961e35384909fb897d568a475585ee5d752dc5e4
ericcooper1 <67962236+ericcooper1@users.noreply.github.com>
2020-07-10 10:14:19 -0400

    Add files via upload

---
+++ hhhhhooootttt.py
@@ -0,0 +1,13 @@
+temp = 100
+if temp >= 120:
+    feel = 'deadly'
+elif temp >= 95:
+    feel = 'hot'
+elif temp >= 80:
+    feel = 'warm hot'
+elif temp >= 77:
+    feel = 'warm'
+elif temp >= 66:
+    feel = 'cool'
+
+print("the feel is ", feel)
